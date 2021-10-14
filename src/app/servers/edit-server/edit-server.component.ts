b98b8f61dd6cf94cb6845996e395b64ecad782c0
Aytugan Özgan <aytgnozgn@gmail.com>
2021-10-14 10:18:53 +0300

    146. video sonu

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -1,22 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Params } from "@angular/router";
 
-import { ServersService } from '../servers.service';
+import { ServersService } from "../servers.service";
 
 @Component({
-  selector: 'app-edit-server',
-  templateUrl: './edit-server.component.html',
-  styleUrls: ['./edit-server.component.css']
+  selector: "app-edit-server",
+  templateUrl: "./edit-server.component.html",
+  styleUrls: ["./edit-server.component.css"],
 })
 export class EditServerComponent implements OnInit {
-  server: { id: number, name: string, status: string };
-  serverName = '';
-  serverStatus = '';
+  server: { id: number; name: string; status: string };
+  serverName = "";
+  serverStatus = "";
+  allowEdit = false;
 
-  constructor(private serversService: ServersService, private route: ActivatedRoute) { }
+  constructor(
+    private serversService: ServersService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.route.queryParams.subscribe();
+    this.route.queryParams.subscribe((queryParams: Params) => {
+      this.allowEdit = queryParams["allowEdit"] == 1 ? true : false;
+    });
     this.route.fragment.subscribe();
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
@@ -24,7 +30,9 @@ export class EditServerComponent implements OnInit {
   }
 
   onUpdateServer() {
-    this.serversService.updateServer(this.server.id, { name: this.serverName, status: this.serverStatus });
+    this.serversService.updateServer(this.server.id, {
+      name: this.serverName,
+      status: this.serverStatus,
+    });
   }
-
 }
